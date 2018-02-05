8f3e624baea63d2309c00a17884cca00d345e7ee
elitam <34102415+elitam@users.noreply.github.com>
2018-02-05 22:11:04 +0200

    Add files via upload

---
+++ 02 _ Massives and Arrays/02 _ Sum of Last(k) Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02___Sum_of_Last_k__Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int[] nums = new int[n];
+            nums[0]=1;
+            for (int i = 1; i < n; i++)
+            {
+                var sum = 0;
+                for (int j = 1; j <= k; j++)
+                {
+                    sum += i - j >= 0 ? nums[i - j] : 0;
+                }
+                nums[i] = sum;
+            }
+            Console.WriteLine(String.Join(" ", nums));
+        }
+    }
+}
---
+++ 02 _ Massives and Arrays/_sample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _sample
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();  
+            var count = nums.Length;
+            int[] reversed = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                reversed[i] = nums[nums.Length - 1 - i];
+            }
+            Console.WriteLine(String.Join(" ", reversed));
+
+        }
+
+        private static void Method1()
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            for (int i = 0; i < nums.Length / 2; i++)
+            {
+                int temporary = nums[nums.Length - 1 - i];
+                nums[nums.Length - 1 - i] = nums[i];
+                nums[i] = temporary;
+            }
+            Console.WriteLine(String.Join(" ", nums));
+        }
+    }
+}
